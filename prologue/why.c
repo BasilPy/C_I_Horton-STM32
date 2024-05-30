68854bf9689d0a898de2de3b5e7c8e788e842908
Basil Su <sudobasil@gmail.com>
2024-05-31 01:20:38 +0200

    prologue, why?

---
+++ prologue/why.c
@@ -0,0 +1,17 @@
+/* Basil Su */
+#include <stdio.h>
+
+int main(void){
+	printf("This is a diary.\n");
+	printf("827\n")
+	printf("Who am I?\n");
+	printf("I am a person from Russia and ethnically russian. \nI am against the war in Ukraine which my country started.\n");
+	printf("I left Russia almost two years ago, and \nI need to continue to be alive.\n");
+	printf("While I'm writing those sentences\nI am aware a lot of people are dying\nand a lot of refugees from Ukraine in Germany, \nin other counties, including their own, \n\
+are suffering from unknown future \nand from the inability to provide basic needs to themselfs.\n");
+	printf("I am full of empathy toward Ukrainians,\nand do not understand rossijan\n(general name of russian sinizens) who take up arms.\n");
+	printf("At the moment I have low self-esteem in terms of career, \nso I am doing it for myself. Yes, It is a bit selfish, \n\
+and don't know if it helps me,\nbut I'll try to educate myself with hope for a better future. \n");
+	printf(" 🕊️ \n");
+	return 0;
+}
