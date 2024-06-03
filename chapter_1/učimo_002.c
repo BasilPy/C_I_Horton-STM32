3ad0a7aa7078c3f5a571284a130fe0514f4a8671
Basil Su <sudobasil@gmail.com>
2024-06-04 00:36:26 +0200

    srpski

---
+++ chapter_1/učimo_002.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	printf("1. Nominativ - Ko? šta?\t\tdrug\tprozor\n");
+	printf("2. Genitiv - Koga? čega?\tdruga\tprozora\n");
+	printf("3. Dativ - Kome? čemu?\t\tdrugu\tprozoru\n");
+	printf("4. Akuzativ - Koga? šta? kuda?\tdruga\tprozor\n"); // u koga?
+	printf("5. Vokativ - poziv za\t\tdruže\t!?prozore\n");
+	printf("6. Instrumental - Kime? čime?\tdrugom\tprozorom\n"); // ca kime?
+	printf("7. Lokativ - O kome?O čemu?Gde?\tdrugu\tprozoru\n"); // na kome? na čemu?
+	return 0;
+}
---
+++ chapter_1/učimo_srpski_001.c
@@ -0,0 +1,25 @@
+// Basil Su
+#include <stdio.h>
+
+int main(void){
+	printf("I can't live a blank page,\n but I don't have much progress with C,\n million excuses\n");
+	printf("jesam = biti\n");
+	printf("ja sam odlično\n");
+	printf("ja sam Basil\n");
+	printf("Ja sam električar\n");
+	printf("Ja sam in inženjer\n");
+	printf("Ja sam tvoj drug\n");
+	printf("Ti si moj drug | Ti si lekar\n");
+	printf("Mi smo električari\n");
+	printf("Vi ste njegov drug\n");
+	printf("Oni su drugari\n");
+	printf("Ovo je igračka\n");
+	printf("Ona je drugarica\n");
+	printf("On je drug\n\n");
+	int količina = 0;
+	int *p = &količina;
+	*p = 2;
+	printf("Ja imam %d drugovi\n", *p);
+	printf("Ti imaš druga\n")
+	return 0;
+}
